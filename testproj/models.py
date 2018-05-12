a86bf1f7b9e2b840e65ee8e052b425408e867063
kazanzhy <dkazanzhy@gmail.com>
2018-05-12 18:36:25 +0300

    New code)

--- testproj/models.py
+++ testproj/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class IdeaStatus(models.Model):
+    '''
+    Статусы идей. Каждая трока - один статус (только создано, подтверждено, отклонено)
+    '''
+    status = models.CharField(unique=True)
+    class Meta:
+        db_table = 'statuses'
+
+
+class Role(models.Model):
+    '''
+    Роли пользователей. Каждая трока - одна роль (архитектор, модератор, пользователь, забаненный)
+    '''
+    status = models.CharField(unique=True)
+    class Meta:
+        db_table = 'roles'
+
+
+class Extenden_User(models.Model):
+    '''
+    База пользователей. Расширяет стандартную модель
+    '''
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # Standart User model
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    is_verified = models.BooleanField()
+    class Meta:
+        db_table = 'users'
+
+
+class Ideas(models.Model):
+    '''
+    Идеи. Каждая строка - одна идея
+    '''
+    title = models.CharField() #unique=True) ???
+    cover = models.ImageField(upload_to = 'uploads/%Y/%m/%d/', blank=True)
+    content = models.TextField()
+    status = models.ForeignKey(IdeaStatus, on_delete=models.CASCADE)
+    author = models.ForeignKey(Extenden_User, on_delete=models.CASCADE)
+    moderator = models.ForeignKey(Extenden_User, on_delete=models.CASCADE)
+    create_date = models.DateTimeField()
+    edit_date = models.DateTimeField()
+    class Meta:
+        db_table = 'ideas'
+
+
+class Likes(models.Model):
+    '''
+    Лайки. Каждая строка - лайк одного пользователя одной идее
+    '''
+    user = models.ForeignKey(Extenden_User, on_delete=models.CASCADE)
+    idea = models.ForeignKey(Ideas, on_delete=models.CASCADE)
+    create_date = models.DateTimeField()
+    class Meta:
+        db_table = 'likes'
+
+
+class Views(models.Model):
+    '''
+    Просмотры. Каждая строка - просмотр одной идеи одним пользователем
+    '''
+    user = models.ForeignKey(Extenden_User, on_delete=models.CASCADE)
+    idea = models.ForeignKey(Ideas, on_delete=models.CASCADE)
+    create_date = models.DateTimeField()
+    class Meta:
+        db_table = 'views'
+
+
